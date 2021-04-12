ceb989a2087bcf79136be23ebc0652937a1c875e
KhaliullinArR <74345012+KhaliullinArR@users.noreply.github.com>
2021-04-12 22:55:55 +0300

    added Commit

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        try {
+            if(fileName.endsWith(".xml")){
+                Properties properties = new Properties();
+                properties.loadFromXML(new FileInputStream(fileName));
+                return properties;
+            }
+            Properties properties = new Properties();
+            properties.load(new FileReader(fileName));
+            return properties;
+        } catch (IOException e) {
+        }
+        return new Properties();
+    }
+}
