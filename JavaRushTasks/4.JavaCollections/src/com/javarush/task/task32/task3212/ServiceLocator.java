442870ee51bc1279cc904d40b04122c92d63c158
KhaliullinArR <74345012+KhaliullinArR@users.noreply.github.com>
2021-04-18 21:07:42 +0300

    commited
    JSON

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
@@ -20,6 +20,13 @@ public class ServiceLocator {
      * @return Object mapped to the name in context
      */
     public static Service getService(String jndiName) {
-        return null;
+        Service service = cache.getService(jndiName);
+        if (service != null) {
+            return service;
+        }
+        InitialContext initialContext = new InitialContext();
+        Service service1 = (Service) initialContext.lookup(jndiName);
+        cache.addService(service1);
+        return service1;
     }
 }
